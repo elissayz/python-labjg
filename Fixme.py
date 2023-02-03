71c0ec223500139c472c3d7ad5dc8d37f0c6cf88
Meghna Pamula <mpamula25@cmc.edu>
2023-02-03 14:54:47 -0800

    fixed so test cases pass

--- Fixme.py
+++ Fixme.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
+import Fixme
+import pytest
+  
+  
 def evens(n):
-    '''
-    Returns a list of even numbers from 0 to n inclusive.
-    '''
+    arr = range(n+1)
+    arr = filter(lambda x: (x % 2 == 0), arr)
+    return list(arr)
